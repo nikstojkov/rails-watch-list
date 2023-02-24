b172a922f734887b8df143351841e2bd9a6cbda5
Nik Stojkov <nik.stojkov@gmail.com>
2023-02-24 15:31:12 +0000

    trying to commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
+  resources :lists do
+    resources :bookmarks
+  end
+  resources :bookmarks, only: [:destroy]
+  resources :movies
 end
